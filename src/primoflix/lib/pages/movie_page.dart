2da4948b72da8ab6c3924d776d6c254dbe82be1a
david <david.emanoel.706@ufrn.edu.br>
2023-06-18 18:10:44 -0300

    pages

---
+++ src/primoflix/lib/pages/movie_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class MoviePage extends StatelessWidget {
+  const MoviePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Página de filmes..."),
+      ),
+    );
+  }
+}
