c41163129f7ceb63957a303c0f077990b10710af
david <david.emanoel.706@ufrn.edu.br>
2023-06-24 18:34:43 -0300

    update

--- src/primoflix/lib/widgets/section_view.dart
+++ src/primoflix/lib/widgets/section_view.dart
@@ -33,8 +33,8 @@ class _SectionViewState extends State<SectionView> {
                     padding: const EdgeInsets.all(8.0),
                     child: Container(
                       color: Colors.white,
-                      width: 100,
-                      height: 50,
+                      width: 120,
+                      height: 150,
                     ),
                   );
                 }),
