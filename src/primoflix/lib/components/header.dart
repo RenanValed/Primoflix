e10433bb887de1ba61484e82af73a5243a63d378
david <david.emanoel.706@ufrn.edu.br>
2023-06-24 18:34:22 -0300

    update

--- src/primoflix/lib/components/header.dart
+++ src/primoflix/lib/components/header.dart
@@ -10,11 +10,11 @@ class Header extends StatelessWidget{
       crossAxisAlignment: CrossAxisAlignment.center,
       textBaseline: TextBaseline.alphabetic,
       children: <Widget>[
-        Icon(Icons.menu, color: Color(0xFFF2F2F2),),
+        const Icon(Icons.menu, color: Color(0xFFF2F2F2),),
         Image.asset('resources/imagens/PRIMOFLIX_LOGO.png'),
         CircleAvatar(
           radius: 26,
-          backgroundImage: NetworkImage(
+          backgroundImage: const NetworkImage(
             'https://avatars.githubusercontent.com/u/86006755?s=400&u=f9cc0927106de2d83cc1e2bd36f27e0936b8ae84&v=4',    
           ),
         ),
