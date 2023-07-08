a490609afba5227b45f47787eb5899af34cd48bf
david <david.emanoel.706@ufrn.edu.br>
2023-07-08 20:42:38 -0300

    Added  GetX obs

--- src/primoflix/lib/widgets/relevant_movie_list_view.dart
+++ src/primoflix/lib/widgets/relevant_movie_list_view.dart
@@ -1,29 +1,33 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:primoflix/data/upComing_movie.dart';
+import 'package:primoflix/data/upComingMovieModelController.dart';
 
 import '../components/relevant_card.dart';
 
-class RelevantMoviesListView extends StatelessWidget{
-  late Future<UpcomingMovie> upComingMovie;
+class RelevantMoviesListView extends StatelessWidget {
   
-  List<String> movies = [
-    "https://upload.wikimedia.org/wikipedia/commons/6/64/Collage_of_Six_Cats-02.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/6/64/Collage_of_Six_Cats-02.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/6/64/Collage_of_Six_Cats-02.jpg"
-  ];
+  UpcomingMovieController upcomingMovieController = Get.put(UpcomingMovieController());
 
   @override
   Widget build(BuildContext context){
     return  SizedBox(
       height: 200,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: movies.length,
-        itemBuilder: (context, index) => RelevantCard(image: movies[index], id:0),
-      ),
-  );
+      child: 
+        Obx(()=> upcomingMovieController.isLoading.value ?
+          Center(child: CircularProgressIndicator(),)
+          :
+          ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: upcomingMovieController.upcomingMovieModel?.assets?.length ?? 0,
+            itemBuilder: 
+              (context, index) => 
+                RelevantCard(
+                  image: upcomingMovieController.upcomingMovieModel!.assets![index].poster_path,
+                  id: upcomingMovieController.upcomingMovieModel!.assets![index].id,
+                ),
+          ),
+        )
+    );
   } 
 }
