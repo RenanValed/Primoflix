05b9ff628121a945ca740a1a3e5e5978747b96e0
david <david.emanoel.706@ufrn.edu.br>
2023-07-08 21:14:06 -0300

    fetch data update

--- src/primoflix/lib/data/fetchData.dart
+++ src/primoflix/lib/data/fetchData.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
-import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 dynamic fetchData(String url) async{
