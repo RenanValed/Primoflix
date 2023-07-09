40341022d1b2f37c3538afdd3fb6918c3651c611
david <david.emanoel.706@ufrn.edu.br>
2023-07-08 21:15:21 -0300

    using generic request

--- src/primoflix/lib/data/upComingMovieModelController.dart
+++ src/primoflix/lib/data/upComingMovieModelController.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
 import 'package:primoflix/data/upComingMoviesModel.dart';
+
+import 'fetchData.dart';
 
 class UpcomingMovieController extends GetxController{
   var isLoading = false.obs;
@@ -13,30 +10,10 @@ class UpcomingMovieController extends GetxController{
   @override
   Future<void> onInit() async{
     super.onInit();
-    fetchData();
+    isLoading(true);
+    var res = await fetchData('movie/upcoming');
+    upcomingMovieModel = UpcomingMovieModel.fromJson(res);
+    isLoading(false);
+    
   }
-
-  fetchData() async{
-    try{
-      isLoading(true);
-      http.Response response = await http.get( Uri.tryParse(
-          'https://api.themoviedb.org/3/movie/upcoming?api_key=0fffac71fbe41c4a03797e90ed24dbcb')!
-      );
-      // debugPrint('prev-Request');
-      if (response.statusCode == 200){
-        // Requisição deu certo...
-        debugPrint('Request');
-        var responseJson = jsonDecode(response.body);
-        upcomingMovieModel = UpcomingMovieModel.fromJson(responseJson);
-      } else {
-        debugPrint('Error fetching data');
-      }
-
-    } catch(e){
-      debugPrint('Error while getting data: $e');
-    } finally{
-      isLoading(false);
-    }
-  } 
-
 }
