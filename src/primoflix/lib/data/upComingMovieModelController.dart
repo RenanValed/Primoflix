a851dd497269e9069bed4a7ae5992382034ffb83
david <david.emanoel.706@ufrn.edu.br>
2023-07-10 10:24:25 -0300

    updates

--- src/primoflix/lib/data/upComingMovieModelController.dart
+++ src/primoflix/lib/data/upComingMovieModelController.dart
@@ -1,18 +1,18 @@
 import 'package:get/get.dart';
-import 'package:primoflix/data/upComingMoviesModel.dart';
+import 'package:primoflix/data/MovieModel.dart';
 
 import 'fetchData.dart';
 
 class UpcomingMovieController extends GetxController{
   var isLoading = false.obs;
-  UpcomingMovieModel? upcomingMovieModel;
+  MovieModel? movieModel;
 
   @override
   Future<void> onInit() async{
     super.onInit();
     isLoading(true);
     var res = await fetchData('movie/upcoming');
-    upcomingMovieModel = UpcomingMovieModel.fromJson(res);
+    movieModel = MovieModel.fromJson(res);
     isLoading(false);
     
   }
